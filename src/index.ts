6f5fe4e42a3632faa076f85a18a4334a65b7b6d1
usamashabbir4321 <160167753+usamashabbir4321@users.noreply.github.com>
2024-03-08 08:33:25 +0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+let wssServerIp: string;
+let uriServerIp: string;
+let diallingURI: string;
+let sipExtension: string;
+let extensionPassword: string;
+let enable_sip_logs: boolean;
+let enableLogs: boolean;
+let IP: string;
+let dialerURI: string;
+let sipPassword: string;
+
+export function widgetConfigs(
+  ccmUrl: string,
+  widgetIdentifier: string,
+  callback: (data: any) => void
+) {
+  fetch(`${ccmUrl}/widget-configs/${widgetIdentifier}`)
+    .then((response) => response.json())
+    .then((data) => {
+      callback(data);
+      wssServerIp = data.webRtc.wssFs;
+      uriServerIp = data.webRtc.uriFs;
+      diallingURI = data.webRtc.diallingUri;
+      sipExtension = data.webRtc.sipExtension;
+      extensionPassword = data.webRtc.extensionPassword;
+      enable_sip_logs = data.webRtc.enabledSipLogs;
+      enableLogs = enable_sip_logs;
+      IP = uriServerIp;
+      dialerURI = "sip:" + diallingURI + "@" + uriServerIp;
+      sipPassword = extensionPassword;
+    });
+}
+
+/**
+ * Get Pre Chat Form
+ * @param formUrl
+ * @param formId
+ * @param callback
+ */
+export function getPreChatForm(
+  formUrl: string,
+  formId: string,
+  callback: (data: any) => void
+) {
+  fetch(`${formUrl}/forms/${formId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      callback(data);
+    });
+}
+
+/**
+ * Form Validation
+ * @param formUrl
+ * @param callback
+ */
+export function formValidation(formUrl: string, callback: (data: any) => void) {
+  fetch(`${formUrl}/formValidation`)
+    .then((response) => response.json())
+    .then((data) => {
+      callback(data);
+    });
+}
